f636964a2a8bf5887f61673a5b08e9e016a7485b
Leo T <leonidtodosiev@gmail.com>
2021-03-22 12:01:41 +0100

    first commit

---
+++ learn-python-freecodecamp/mad_libs_game.py
@@ -0,0 +1,8 @@
+adjective1 = input("Enter an adjective: ")
+adjective2 = input("Enter another adjective: ")
+type_of_bird = input("Enter a type of bird: ")
+room = input("Enter a room: ")
+
+print("It was a " + adjective1 + ", cold November day. ")
+print("I woke up to the " + adjective2 + " smell of " + type_of_bird)
+print("roasting in the " + room + " downstairs.")
